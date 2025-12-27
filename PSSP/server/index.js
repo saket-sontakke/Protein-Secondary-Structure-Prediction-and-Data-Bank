e21f567c2c50717199892c2d2b3703705626eac2
saket-sontakke <sontakkesaket9@gmail.com>
2025-12-27 17:10:04 +0530

    Update index.js

--- PSSP/server/index.js
+++ PSSP/server/index.js
@@ -129,7 +129,7 @@ app.use((req, res) => {
 });
 
 // --- DEPLOYMENT CHANGE: Added fallback port ---
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
 });
